8c2554b90f69f937f46eed90a9b9dbd92b25f1db
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2024-01-14 17:30:56 +0100

    primer commit

---
+++ src/test/java/com/esliceu/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esliceu.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

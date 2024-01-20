18575178511f52b98cd4f05ba293aa343f3448d7
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2024-01-20 21:09:10 +0100

    hem afegit els genre a el filtre

--- src/main/java/com/esliceu/movies/Entities/Genre.java
+++ src/main/java/com/esliceu/movies/Entities/Genre.java
@@ -9,33 +9,25 @@ public class Genre {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "genre_id")
     private Long genreId;
-
     @Column(name = "genre_name")
     private String genreName;
-
     @OneToMany(mappedBy = "genre")
     private Set<MovieGenres> movieGenres;
-
     public Long getGenreId() {
         return genreId;
     }
-
     public void setGenreId(Long genreId) {
         this.genreId = genreId;
     }
-
     public String getGenreName() {
         return genreName;
     }
-
     public void setGenreName(String genreName) {
         this.genreName = genreName;
     }
-
     public Set<MovieGenres> getMovieGenres() {
         return movieGenres;
     }
-
     public void setMovieGenres(Set<MovieGenres> movieGenres) {
         this.movieGenres = movieGenres;
     }
--- src/main/java/com/esliceu/movies/Repos/MovieSearchRepo.java
+++ src/main/java/com/esliceu/movies/Repos/MovieSearchRepo.java
@@ -17,8 +17,6 @@ public interface MovieSearchRepo extends JpaRepository<Movie, Long> {
               "JOIN Person p ON p.personId = mc.person.personId " +
               "WHERE mc.job = 'Author' AND p.personName = :personName")
       List<Movie> findMovieByActor(@Param("personName") String keyword);
-
-
      */
     @Query("SELECT m FROM Movie m " +
             "JOIN MovieCast mc ON m.movieId = mc.movie.movieId " +
@@ -27,7 +25,6 @@ public interface MovieSearchRepo extends JpaRepository<Movie, Long> {
     List<Movie> findMovieByActor(@Param("personName") String keyword);
 
 
-   // @Query("SELECT m FROM Movie m WHERE m.title LIKE %:keyword%")
     @Query("SELECT m FROM Movie m WHERE title LIKE %:keyword%")
     //Page<Movie> findMoviesByTitle(@Param("keyword")String keyword, Pageable pageable);
     List<Movie> findMoviesByTitle(@Param("keyword")String keyword);

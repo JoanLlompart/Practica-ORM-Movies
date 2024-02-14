c0574377059d1e38f56760d2768b9fdd28a53f1b
JoanLlompart <jllompartsocias@cifpfbmoll.eu>
2024-02-14 12:50:17 +0100

    Millorant

--- src/main/java/com/esliceu/movies/Controllers/AdminAddController.java
+++ src/main/java/com/esliceu/movies/Controllers/AdminAddController.java
@@ -45,84 +45,113 @@ public class AdminAddController {
     @PostMapping("/adminArea/add/language")
     public ResponseEntity<Object> adminAddPostLanguage(HttpServletRequest req, HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        String isoCode = data.get("value1");
-        String nameCountry = data.get("value2");
-        String successMessage =adminAddServices.insertLanguage(isoCode,nameCountry);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String isoCode = data.get("value1");
+            String nameCountry = data.get("value2");
+            String successMessage = adminAddServices.insertLanguage(isoCode, nameCountry);
+            return ResponseEntity.ok().body(successMessage);
+        }
+        return ResponseEntity.ok().body("Acces denied, you do not have admin permissions");
     }
 
     @PostMapping("/adminArea/add/languageRole")
     public ResponseEntity<Object> adminAddPostLanguageRole( HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        String successMessage = adminAddServices.insertLanguageRole(data);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String successMessage = adminAddServices.insertLanguageRole(data);
+            return ResponseEntity.ok().body(successMessage);
+        }
+        return ResponseEntity.ok().body("Acces denied, you do not have admin permissions");
     }
 
     @PostMapping("/adminArea/add/genre")
     public ResponseEntity<Object> adminAddPostGenre( HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        String successMessage =adminAddServices.insertNewGenre(data);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String successMessage = adminAddServices.insertNewGenre(data);
+            return ResponseEntity.ok().body(successMessage);
+        }
+        return ResponseEntity.ok().body("Acces denied, you do not have admin permissions");
     }
 
     @PostMapping("/adminArea/add/keyword")
     public ResponseEntity<Object> adminAddPostKeyword( HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        String successMessage =adminAddServices.insertNewKeyword(data);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String successMessage = adminAddServices.insertNewKeyword(data);
+            return ResponseEntity.ok().body(successMessage);
+        }
+        return ResponseEntity.ok().body("Acces denied, you do not have admin permissions");
     }
 
     @PostMapping("/adminArea/add/productionCompany")
     public ResponseEntity<Object> adminAddPostProductionCompany( HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        String successMessage =adminAddServices.insertNewProductionCompany(data);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String successMessage = adminAddServices.insertNewProductionCompany(data);
+            return ResponseEntity.ok().body(successMessage);
+        }
+        return ResponseEntity.ok().body("Acces denied, you do not have admin permissions");
     }
 
 
     @PostMapping("/adminArea/add/gender")
     public ResponseEntity<Object> adminAddPostGender( HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        String successMessage =adminAddServices.insertNewGender(data);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String successMessage = adminAddServices.insertNewGender(data);
+            return ResponseEntity.ok().body(successMessage);
+        }
+        return ResponseEntity.ok().body("Acces denied, you do not have admin permissions");
     }
 
     @PostMapping("/adminArea/add/department")
     public ResponseEntity<Object> adminAddPostDepartment( HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        String successMessage =adminAddServices.insertNewDepartment(data);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String successMessage = adminAddServices.insertNewDepartment(data);
+            return ResponseEntity.ok().body(successMessage);
+        }
     }
 
     @PostMapping("/adminArea/add/person")
     public ResponseEntity<Object> adminAddPostPerson( HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        //userServices.setEmail(email);
-        String successMessage =adminAddServices.insertNewPerson(data);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String successMessage = adminAddServices.insertNewPerson(data);
+            return ResponseEntity.ok().body(successMessage);
+        }
+        return ResponseEntity.ok().body("Acces denied, you do not have admin permissions");
     }
 
     @PostMapping("/adminArea/addDirector")
     public ResponseEntity<Object> adminAddDirector( HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        //userServices.setEmail(email);
-        String successMessage =movieCrewServices.addMovieDirector(data);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String successMessage = movieCrewServices.addMovieDirector(data);
+            return ResponseEntity.ok().body(successMessage);
+        }
+        return ResponseEntity.ok().body("Acces denied, you do not have admin permissions");
+
     }
 
     @PostMapping("/adminArea/addGenre")
     public ResponseEntity<Object> adminAddGenre( HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        //userServices.setEmail(email);
-        String successMessage =movieGenresServices.addMovieGenre(data);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String successMessage = movieGenresServices.addMovieGenre(data);
+            return ResponseEntity.ok().body(successMessage);
+        }
+        return ResponseEntity.ok().body("Acces denied, you do not have admin permissions");
     }
     @PostMapping("/adminArea/addKeyword")
     public ResponseEntity<Object> adminAddMovieKeyword( HttpSession session , @RequestBody Map<String,String> data) {
         String email = (String) session.getAttribute("email");
-        //userServices.setEmail(email);
-        String successMessage =movieKeywordsServices.addMovieKeyword(data);
-        return ResponseEntity.ok().body(successMessage);
+        if (email != null) {
+            String successMessage = movieKeywordsServices.addMovieKeyword(data);
+            return ResponseEntity.ok().body(successMessage);
+        }
+        return ResponseEntity.ok().body("Acces denied, you do not have admin permissions");
     }
 
 }
